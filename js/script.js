8ca8ad879a913f8a3163cca0c4a7e5f71f2a8e02
kiomafernandes <kioma_sousa@hotmail.com>
2023-08-06 19:21:58 -0300

    git --commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const dia = document.getElementById('dia');
+const mes = document.getElementById('mes');
+const ano = document.getElementById('ano')
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (sec < 10) sec = '0' + sec;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent =sec;
+
+    let day = dateToday.getDate();
+    let month = dateToday.getMonth() +1 ;
+    let year = dateToday.getFullYear();
+
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
+    
+
+    dia.textContent = day;
+    mes.textContent = month;
+    ano.textContent = year;
+})
+
